40e6e0e345b11c5e95d49d53fbb72d2968ab10e0
Milton Carlos Katoo <mckatoo@gmail.com>
2026-01-07 22:40:08 -0300

    update astronvim config

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      -- NOTE: The log_level is in `opts.opts`
+      opts = {
+        log_level = "DEBUG", -- or "TRACE"
+      },
+    },
+    config = function()
+      require("codecompanion").setup {
+        opts = {
+          log_level = "DEBUG", -- or "TRACE"
+        },
+      }
+    end,
+  },
+}
